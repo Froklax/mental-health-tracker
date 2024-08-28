d84c08d0644a98548601044af04cdf7add110bfb
Froklax <bertrand.gwynfory@gmail.com>
2024-08-28 14:08:05 +0700

    added url deployment pws to allowed-hosts

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2dw=j0jylm&krif1o)hp+fe5ne%y6^j-tug7tz=-)hox)t3snj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "bertrand-gwynfory-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
 
 # Application definition
