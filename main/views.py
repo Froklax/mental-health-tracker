b01736c477cb2d7452300dc5747974ac3ec80b8d
Bertrand Gwynfory Iskandar <bertrand.gwynfory@gmail.com>
2024-09-16 00:26:08 +0700

    change wrong name

--- main/views.py
+++ main/views.py
@@ -8,9 +8,9 @@ def show_main(request):
     mood_entries = MoodEntry.objects.all()
 
     context = {
-        'name': 'Pak Bepe',
-        'class': 'PBP D',
-        'npm': '2306123456',
+        'name': 'Bertrand Gwynfory Iskandar',
+        'class': 'PBP C',
+        'npm': '2306152121',
         'mood_entries': mood_entries
     }
 
